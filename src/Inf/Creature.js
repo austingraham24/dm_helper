5e11035b442020a8cf0872009bc69647c50c866a
austingraham24 <austingraham24@gmail.com>
2018-03-11 21:48:50 -0500

    starting creature object

--- src/Inf/Creature.js
+++ src/Inf/Creature.js
@@ -1,11 +1,15 @@
 
 
 const Creature = {
+	//
 	const create = (name, type, classification, alignment, challenge_rating, xp, 
 		profficiency, ac, size, hp, laguages, stats, senses, skills, save_dc, attack_bonus, dpr,
 		vulnerabilities, resistances, immunities, weapons, actions) => {
 
 	}
+
+	//
+	const 
 };
 
 export default Creature;
