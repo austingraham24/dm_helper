57efd5fbed75de2e33479614a7efac9599382884
austingraham24 <austingraham24@gmail.com>
2018-03-14 23:30:31 -0500

    documenting the select component and using prop-types

--- src/Components/SelectField.js
+++ src/Components/SelectField.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {FormControl} from "react-bootstrap";
+import PropTypes from 'prop-types';
 
-//expects array as optionArray of string options
-const CreatureClassificationSelect = (props) => {
+const SelectField = (props) => {
+	if (!props.arrayData && !props.objectData) {
+		return null
+	}
 
 	function hasPlaceholder() {
 		if (!props.placeholder) {
@@ -39,5 +42,17 @@ const CreatureClassificationSelect = (props) => {
 	);
 }
 
+SelectField.propTypes = {
+	/*REQUIRED*/
+  name: PropTypes.string.isRequired, //gives the select field a name; useful use in the onChange methode to know which piece of state to change
+  onChange: PropTypes.func.isRequired, //the method to call when the field is changed
 
-export default CreatureClassificationSelect;
+  /*OPTIONAL*/
+  arrayData: PropTypes.arrayOf(PropTypes.string), //a list of simple strings where each is a unique and individual option
+  objectData: PropTypes.object, //an object {value:display} where value is used as the key and value and the display is what is shown to the user
+  /*note: one of the above must not be null in order for the compoenent to render*/
+  placeholder: PropTypes.string, //a hidden option to be shown as a default rather than the first item in the array/object
+  stateValue: PropTypes.string, //the value the current form state has; if null the field defaults to value=""
+}
+
+export default SelectField;
